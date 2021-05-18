8f3bf1e339d4b51ea95b0dcc67234080c57aa43c
Lorenzo Panieri <panierilorenzo@gmail.com>
2021-05-18 23:00:40 +0200

    trying things

--- src/core/non_lin_filters.rs
+++ src/core/non_lin_filters.rs
@@ -15,7 +15,7 @@ impl SlewClip1 {
     pub fn new() -> Self {
         Self {
             diff: Diff::new(),
-            int:  LeakyInt::new(0.1, 0.0),
+            int:  LeakyInt::new(0.02, 0.0),
             sr_scale: 1.0,
             dt_scale: 1.0,
             hardness: 0.5,
@@ -34,10 +34,10 @@ impl Process<f64> for SlewClip1 {
         let post_gain = 1.0 - self.drive;
         let pre_gain  = 1.0 / post_gain;
 
-        let dx = self.diff.step(input) * self.sr_scale;
+        let dx = self.diff.step(input); //* self.sr_scale;
         //let dx_sat = var_clip(dx * pre_gain, self.hardness);
         //self.int.step(dx_sat * post_gain) * self.dt_scale
-        self.int.step(dx) * self.dt_scale
+        self.int.step(dx) //* self.dt_scale
     }
 }
 
