3731311bd0a37bc680a620fb49df1c91c6417129
XYdamage <xydamage@gmail.com>
2021-10-03 19:38:42 +0200

    added gain to decibels conversion

--- src/utils/conversion.rs
+++ src/utils/conversion.rs
@@ -5,6 +5,9 @@ use std::f64::consts;
 /// Turn decibels to gain
 pub fn db_to_gain(db: f64) -> f64 { 10.0_f64.powf( db * 0.05 ) }
 
+/// Turn gain to decibels
+pub fn gain_to_db(gain: f64) -> f64 { 20.0 * gain.abs().log10() }
+
 /// Normalize frequencies to the range [0; pi] for use in filters.
 pub fn f_to_omega(f: f64, sr: f64) -> f64 { consts::TAU * f / sr }
 
